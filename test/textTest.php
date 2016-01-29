c565a1a0d84532cbb19ec878a85a23371a359ed1
koluku <korufiru@gmail.com>
2016-01-29 21:12:27 +0900

    Travis用の設定ファイルの追加
    動くかどうかは知らん

---
+++ test/textTest.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <title>Document</title>
+</head>
+<body>
+  <main>
+    <section>
+      <h1>Text</h1>
+      <p>
+        PHPは基本的にHTML上に記述してブラウザ上で実行する。
+      </p>
+      <section>
+        <h2><?php print "Hello, World!"; ?></h2>
+        <p>
+          文字の出力はprintで行うことができる。<br>
+          記法は文字列、数字によって異なる。
+        </p>
+      </section>
+      <section>
+        <h2><?php echo "Write Your Code"; ?></h2>
+        <p>
+          printの代わりにechoで出力することができる。<br>
+          記法はprintと同様。
+        </p>
+      </section>
+      <section>
+        <?php
+          print "<h2>Run on Web</h2>/n";
+          echo "<p>print,echoでHTMLを出力することができる。<br>"¥n;
+          echo "文字列として記述し、HTMLの構造上でタグの最後に¥n（改行）を付けるのが望ましい。</p>";
+         ?>
+      </section>
+    </section>
+  </main>
+</body>
+</html>
